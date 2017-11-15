037fa3df08525ebed62632d980063f30e7730300
Jay Jackson <jaynickjackson@gmail.com>
2017-11-15 10:36:25 -0500

    added NoteEntity

--- ElevenNote.Data/NoteEntity.cs
+++ ElevenNote.Data/NoteEntity.cs
@@ -1,13 +1,32 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ElevenNote.Data
 {
-    class NoteEntity
+    [Table("Note")]
+    public class NoteEntity
     {
+        [Key]
+        public int NoteId { get; set; }
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [MaxLength(500)]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime CreatedUtc { get; set; }
+
+        public DateTime? ModifiedUtc { get; set; }
 
     }
 }
---
+++ ElevenNote.Models/NoteListItemModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteListItemModel
+    {
+        public NoteListItemModel(
+            int noteId,
+            string title,
+            string content,
+            DateTime createUtc,
+            DateTime modifiedUtc
+        )
+        {
+            NoteId = noteId;
+            Title = title;
+            Content = content;
+            CreatedUtc = createUtc;
+            ModifiedUtc = modifiedUtc;
+        }
+
+        public int NoteId { get; }
+
+        public string Title { get; }
+
+        public string Content { get; }
+
+        public DateTime CreatedUtc { get; }
+
+        public DateTime? ModifiedUtc { get; }
+    }
+}
