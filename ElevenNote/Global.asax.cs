543092905fe0e101d2abc2a966980c17c40405b0
Jay Jackson <jaynickjackson@gmail.com>
2017-11-16 16:46:56 -0500

    setup API config

--- ElevenNote/Global.asax.cs
+++ ElevenNote/Global.asax.cs
@@ -14,6 +14,7 @@ namespace ElevenNote
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            WebApiConfig.Register();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
