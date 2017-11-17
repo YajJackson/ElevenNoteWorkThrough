b141e68a729ee276c011c55d249d09b2fe654ee9
Jay Jackson <jaynickjackson@gmail.com>
2017-11-17 09:34:19 -0500

    stubbed out api controller

---
+++ ElevenNote/Controllers/WebAPI/NotesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ElevenNote.Controllers.WebAPI
+{
+    [Authorize]
+    [RoutePrefix("api/Notes")]
+    public class NotesController : ApiController
+    {
+        public bool Put(int id)
+        {
+
+        }
+
+        public bool Delete(int id)
+        {
+
+        }
+    }
+}
