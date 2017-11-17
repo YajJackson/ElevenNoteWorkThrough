f988aefa1dddecf5e4688cf94845f25775545c63
Jay Jackson <jaynickjackson@gmail.com>
2017-11-17 09:58:36 -0500

    added API controller

--- ElevenNote/Controllers/WebAPI/NotesController.cs
+++ ElevenNote/Controllers/WebAPI/NotesController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -11,14 +14,33 @@ namespace ElevenNote.Controllers.WebAPI
     [RoutePrefix("api/Notes")]
     public class NotesController : ApiController
     {
-        public bool Put(int id)
+        private bool SetStarState(int noteId, bool newState)
         {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            var detail = service.GetNoteById(noteId);
 
+            var updatedNote = new NoteEditModel
+            {
+                NoteId = detail.NoteId,
+                Title = detail.Title,
+                Content = detail.Content,
+                IsStarred = newState
+            };
+
+            return service.UpdateNote(updatedNote);
         }
 
+        [Route("{id}/Star")]
+        public bool Put(int id)
+        {
+            return SetStarState(id, true);
+        }
+
+        [Route("{id}/Star")]
         public bool Delete(int id)
         {
-
+            return SetStarState(id, true);
         }
     }
 }
